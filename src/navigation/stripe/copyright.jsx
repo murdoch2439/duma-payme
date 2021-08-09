cf287daa78ee5f0e53e70ae9139449c3d028637a
murdoch2439 <murdochkhallz@gmail.com>
2021-08-09 11:31:44 +0100

    the copyright component

---
+++ src/navigation/stripe/copyright.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Typography, Grid} from '@material-ui/core'
+import NoticeDialog from '../legalNoticePopups/noticeDialog';
+
+const  Copyright = () => {
+  const currentYear =  new Date().getFullYear();
+  return (
+
+    <Grid style={{textAlign:'center',}}>
+        <Typography variant="caption">{`©Copyright-${currentYear}`}</Typography> 
+        <Typography>
+
+          {/* ᛫ */}
+
+        <NoticeDialog separator="&nbsp;|" />
+        </Typography>
+    </Grid>
+  );
+}
+
+
+export default Copyright
