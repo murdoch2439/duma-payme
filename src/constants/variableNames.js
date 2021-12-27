d1aa6478ff27bb517be30e0dbe06c16ae5ca3ca4
murdoch2439 <murdochkhallz@gmail.com>
2021-12-27 07:23:08 +0100

    changing the backend ip address into Dns link

--- src/constants/variableNames.js
+++ src/constants/variableNames.js
@@ -1,13 +1,13 @@
 export const ADMIN_ID_STRING = "adminId"
 export const PAYER_ID_STRING = "payerId"
-export const ONLINE_BACKE_END_PORT = "54.154.208.240"
+export const ONLINE_BACK_END_PORT = "bk.dumapay.blthouse.tech:3000"
 // export const ONLINE_BACKE_END_PORT = "192.168.1.115"
 // export const serverLive = "https://duma-payme-backend.herokuapp.com"
 // export const CURRENCY = "currency"
-export const API_PAYMENT_INIT =  `http://${ONLINE_BACKE_END_PORT}:3001/api/payment-init`
+export const API_PAYMENT_INIT =  `http://${ONLINE_BACK_END_PORT}/api/payment-init`
 // export const API_DUMA_PAY_CLIENT_REDIRECT_INIT=`http://${ONLINE_BACKE_END_PORT}:3001/api/payment-redirect`
-export const API_VALIDATE_PAYMENT_INTENT = `http://${ONLINE_BACKE_END_PORT}:3001/api/validate`
-export const API_CREATE_PAYMENT_INTENT = `http://${ONLINE_BACKE_END_PORT}:3001/api/create-payment-intent`
+export const API_VALIDATE_PAYMENT_INTENT = `http://${ONLINE_BACK_END_PORT}/api/validate`
+export const API_CREATE_PAYMENT_INTENT = `http://${ONLINE_BACK_END_PORT}/api/create-payment-intent`
 // export const API_RETRIEVE_PURCHASED_OBJECT = `http://${ONLINE_BACKE_END_PORT}:3001/api/payment`
 export const DEBIT_CARD = "debit card"
 export const MOBILE_MONEY = "mobile money"
