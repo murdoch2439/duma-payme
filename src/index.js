95ca08a93f7ac7372592299dd45ccd88f55a9b4a
murdoch2439 <murdochkhallz@gmail.com>
2022-02-01 12:32:00 +0100

    code review

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import './i18n'
 import App from './App';
 
-
-// 0978 449 782
-
-// 0824 099 111
-
-// import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
       {/*<Suspense fallback={(<div>Loading¬¬¬</div>)}>*/}
