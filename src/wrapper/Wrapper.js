897c3447e619382d853aaeb228ac66d9640b1bbe
murdoch2439 <murdochkhallz@gmail.com>
2022-02-07 14:44:37 +0100

    Stripe private key refactoring

--- src/wrapper/Wrapper.js
+++ src/wrapper/Wrapper.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 
 
