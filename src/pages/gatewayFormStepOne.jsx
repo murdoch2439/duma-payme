2be5d5bd0f2166f0b60d11882dd4bd67825a72c5
murdoch2439 <murdochkhallz@gmail.com>
2022-02-14 13:51:29 +0100

    back to online config

--- src/pages/gatewayFormStepOne.jsx
+++ src/pages/gatewayFormStepOne.jsx
@@ -85,7 +85,7 @@ const GatewayFormStepOne =()=> {
                 console.log('response Data ====>', responseFromBffPaymentInit.data)
                 setCurrency(responseFromBffPaymentInit.data.currency)
                 setAmount(responseFromBffPaymentInit.data.amount)
-                if((responseFromBffPaymentInit.data.error && responseFromBffPaymentInit.data.code === CODE_403) || responseFromBffPaymentInit.data.error && responseFromBffPaymentInit.data.code === CODE_500){
+                if((responseFromBffPaymentInit.data.error && responseFromBffPaymentInit.data.code === CODE_403) || responseFromBffPaymentInit.data.code === CODE_500){
 
                     dispatch({
                         type: CHANGE_MODAL_STATES,
