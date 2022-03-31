71d118e6ef00d8e156ecee2140ef642128540607
murdoch2439 <murdochkhallz@gmail.com>
2022-03-31 16:38:29 +0100

    Get rid of useless import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React from 'react'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import './App.css';
 import { ThemeProvider } from '@material-ui/styles';
