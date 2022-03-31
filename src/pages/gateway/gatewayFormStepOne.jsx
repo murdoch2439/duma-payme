f5c226fdaf23277238435ca836a3b603a0689db4
murdoch2439 <murdochkhallz@gmail.com>
2022-03-31 17:20:27 +0100

    Removing useless import

--- src/pages/gateway/gatewayFormStepOne.jsx
+++ src/pages/gateway/gatewayFormStepOne.jsx
@@ -46,7 +46,6 @@ const GatewayFormStepOne =()=> {
     const {t} = useTranslation()
 
 
-
     const emailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     useEffect(()=>{
