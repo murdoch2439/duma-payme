16546d11d7913150922ad5767bd730c69a98cc41
murdoch2439 <murdochkhallz@gmail.com>
2022-03-31 17:19:02 +0100

    Removing useless import

--- src/pages/gateway/gatewayFormStepOne.jsx
+++ src/pages/gateway/gatewayFormStepOne.jsx
@@ -7,8 +7,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import {getClientIpAddress, getUrlParams, paymentMethods, responseManager} from '../../utils/helperFunctions';
 import {
-
-
     EDIT_FORM_VALUES,
     USD,
     EUR,
