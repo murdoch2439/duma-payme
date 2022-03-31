74099703d63c3d60b92d054741544363d85b754d
murdoch2439 <murdochkhallz@gmail.com>
2022-03-31 17:20:04 +0100

    Removing useless import

--- src/pages/gateway/gatewayFormStepOne.jsx
+++ src/pages/gateway/gatewayFormStepOne.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Grid,Typography, TextField, FormControl, MenuItem } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { useStateValue } from '../../context';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
