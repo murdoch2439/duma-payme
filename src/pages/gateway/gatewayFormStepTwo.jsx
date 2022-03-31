03b36b62f5fbd88913e6e8654f4e29bfdc1214bd
murdoch2439 <murdochkhallz@gmail.com>
2022-03-31 18:07:22 +0100

    Adding gateway information component

--- src/pages/gateway/gatewayFormStepTwo.jsx
+++ src/pages/gateway/gatewayFormStepTwo.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
-import List from '@material-ui/core/List';
+
 
 
 import { useStateValue } from '../../context';
