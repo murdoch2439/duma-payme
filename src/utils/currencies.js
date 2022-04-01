832a8f3db60ba586dfd5e0997e6d25a8a43c831e
murdoch2439 <murdochkhallz@gmail.com>
2022-04-01 19:16:31 +0100

    Creating currencies enum

--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -1,60 +1,60 @@
-export const Currencies = [
-
-    {
-        "symbol": "CDF",
-        "name": "Congolese Franc",
-        "symbol_native": "FrCD",
-        "decimal_digits": 2,
-        "rounding": 0,
-        "code": "CDF",
-        "name_plural": "Congolese francs"
-    },
-
-    {
-        "symbol": "€",
-        "name": "Euro",
-        "symbol_native": "€",
-        "decimal_digits": 2,
-        "rounding": 0,
-        "code": "EUR",
-        "name_plural": "euros"
-    },
-    {
-        "symbol": "£",
-        "name": "British Pound Sterling",
-        "symbol_native": "£",
-        "decimal_digits": 2,
-        "rounding": 0,
-        "code": "GBP",
-        "name_plural": "British pounds sterling"
-    },
-    {
-        "symbol": "$",
-        "name": "US Dollar",
-        "symbol_native": "$",
-        "decimal_digits": 2,
-        "rounding": 0,
-        "code": "USD",
-        "name_plural": "US dollars"
-    },
-
-    {
-        "symbol": "CFA",
-        "name": "CFA Franc BCEAO",
-        "symbol_native": "CFA",
-        "decimal_digits": 0,
-        "rounding": 0,
-        "code": "XOF",
-        "name_plural": "CFA francs BCEAO"
-    },
-
-    {
-        "symbol": "CAD$",
-        "name": "Canadian Dollar",
-        "symbol_native": "CAD$",
-        "decimal_digits": 0,
-        "rounding": 0,
-        "code": "CAD",
-        "name_plural": "Canadien dollars"
-    }
-]
+// export const Currencies = [
+//
+//     {
+//         "symbol": "CDF",
+//         "name": "Congolese Franc",
+//         "symbol_native": "FrCD",
+//         "decimal_digits": 2,
+//         "rounding": 0,
+//         "code": "CDF",
+//         "name_plural": "Congolese francs"
+//     },
+//
+//     {
+//         "symbol": "€",
+//         "name": "Euro",
+//         "symbol_native": "€",
+//         "decimal_digits": 2,
+//         "rounding": 0,
+//         "code": "EUR",
+//         "name_plural": "euros"
+//     },
+//     {
+//         "symbol": "£",
+//         "name": "British Pound Sterling",
+//         "symbol_native": "£",
+//         "decimal_digits": 2,
+//         "rounding": 0,
+//         "code": "GBP",
+//         "name_plural": "British pounds sterling"
+//     },
+//     {
+//         "symbol": "$",
+//         "name": "US Dollar",
+//         "symbol_native": "$",
+//         "decimal_digits": 2,
+//         "rounding": 0,
+//         "code": "USD",
+//         "name_plural": "US dollars"
+//     },
+//
+//     {
+//         "symbol": "CFA",
+//         "name": "CFA Franc BCEAO",
+//         "symbol_native": "CFA",
+//         "decimal_digits": 0,
+//         "rounding": 0,
+//         "code": "XOF",
+//         "name_plural": "CFA francs BCEAO"
+//     },
+//
+//     {
+//         "symbol": "CAD$",
+//         "name": "Canadian Dollar",
+//         "symbol_native": "CAD$",
+//         "decimal_digits": 0,
+//         "rounding": 0,
+//         "code": "CAD",
+//         "name_plural": "Canadien dollars"
+//     }
+// ]
