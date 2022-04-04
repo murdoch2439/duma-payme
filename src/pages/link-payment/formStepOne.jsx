756ba37035a7675ad9fdeaca4c925b63151a54bb
murdoch2439 <murdochkhallz@gmail.com>
2022-04-04 11:58:17 +0100

    Freezing currencies enum

--- src/pages/link-payment/formStepOne.jsx
+++ src/pages/link-payment/formStepOne.jsx
@@ -19,7 +19,6 @@ const FormStepOne =()=> {
     // if(paymentRequestId){
     //     formValues.payerId = paymentRequestId
 
-
   const receivingAmount = (formValues.clientCurrency === formValues.currency ? formValues.amount : (parseInt(formValues.amount) * parseFloat(formValues.rate)).toFixed(2))
   useEffect(()=>{
           setCurrency(formValues.currency)
