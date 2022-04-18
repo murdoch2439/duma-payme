97d71a6f65da5b6a7ba169b75f9dc0cb0438817c
murdoch2439 <murdochkhallz@gmail.com>
2022-04-18 19:46:59 +0100

    hotfix for publishable key

--- src/wrapper/Wrapper.js
+++ src/wrapper/Wrapper.js
@@ -38,7 +38,6 @@ const Wrapper = () =>{
     useEffect(()=>{
         if(formValues.currency === EMPTY_STRING){
             paymentInitWithBff().then()
-
         }else{
             setCurrency(formValues.currency)
         }
