4771c206b14bf5dc6d50900730c96e2786e6aa0d
murdoch2439 <murdochkhallz@gmail.com>
2022-04-20 15:30:50 +0100

    adding new words for translation

--- src/wrapper/Wrapper.js
+++ src/wrapper/Wrapper.js
@@ -56,7 +56,6 @@ const Wrapper = () =>{
                     setPublicKey(loadStripe(responseFromBffPaymentInit.data.clientKey))
 
                     if((responseFromBffPaymentInit.data.error && responseFromBffPaymentInit.data.code === CODE_403)|| responseFromBffPaymentInit.data.code === CODE_500){
-
                         console.log("response data from init ==> ", responseFromBffPaymentInit.data)
                         dispatch({
                             type: CHANGE_MODAL_STATES,
