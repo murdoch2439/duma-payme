dff7eaa50461894e3bd345f84d2626a2bdcb6f52
murdoch2439 <murdochkhallz@gmail.com>
2022-05-17 14:30:14 +0100

    request service update

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 import {IP_PROVIDER_API_KEY, ONLINE_BACK_END_PORT} from "../constants/variableNames";
 
-
 export const Payment={
-
     baseUrl:axios.create({
         baseUrl:`http://${ONLINE_BACK_END_PORT}:3001/api`
     }),
@@ -12,7 +10,7 @@ export const Payment={
             const {data:{ip}} = await axios.get(`https://api.ipdata.co/?api-key=${IP_PROVIDER_API_KEY}`)
             return ip
         }catch(error){
-            console.log("Did not get the Ip address ==>", error)
+            return console.log("Did not get the Ip address ==>", error)
         }
     },
     init:function(paymentInfo){
