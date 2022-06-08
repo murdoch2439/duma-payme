ed6bab8b99f60330442544913df1b458fcef813f
murdoch2439 <murdochkhallz@gmail.com>
2022-06-08 18:01:57 +0100

    removing useless stuff

--- src/components/gateway-info/index.jsx
+++ src/components/gateway-info/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import {Grid,Typography, List, ListItem, ListItemText, makeStyles} from "@material-ui/core";
 import {totalToPay} from "../../utils/helperFunctions";
 import {useTranslation} from "react-i18next";
+import ConfirmationDialog from "../modals/confirmationModal";
 
 
 
@@ -41,6 +42,10 @@ const useStyles = makeStyles(() => ({
     },
     secondSection:{
         marginRight:10,marginLeft:10,
+    },
+    header:{
+        display:"flex",
+        justifyContent:"space-between",
     }
 }));
 
@@ -52,15 +57,21 @@ const GatewayInfo =({currency, businessObject })=>{
         <Grid className={classes.sectionOne}>
 
             <Grid item xs={12} sm={12} md={12} className={classes.paddingManager} >
-                <Grid item  xs={12}>
-                    <Typography variant="h6">{t("Payment Details")}</Typography>
+                <Grid item xs={12} sm={12} md={12} className={classes.header} display={{ xs: 'inline',sm:'inline', md:'flex'}}>
+                    <Grid item  xs={12}>
+                        <Typography variant="h6">{t("Payment Details")}</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6} style={{ display:'flex', justifyContent:"flex-end"}}>
+                        <ConfirmationDialog />
+                    </Grid>
                 </Grid>
+
                 <List>
                     <ListItem className={classes.listItem} >
                         <ListItemText primary={t('Currency :')} className={classes.key}   />
                         <Typography variant="body1" className={classes.value}>{currency}</Typography>
                     </ListItem>
-                    <ListItem className={[classes.listItem, classes.totalBg]}  >
+                    <ListItem className={classes.listItem} style={{backgroundColor:'#F1F5F6',  borderRadius:5,}}  >
                         <ListItemText primary={t('Total :')} className={classes.key}   />
                         <Typography variant="body1" className={classes.value}>{totalToPay(businessObject)}</Typography>
                     </ListItem>
