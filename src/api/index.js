234f58ab4b897e1229fd05836bcc4281014a1cdf
murdoch2439 <murdochkhallz@gmail.com>
2022-06-08 17:18:40 +0100

    Extract http request

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import {IP_PROVIDER_API_KEY, ONLINE_BACK_END_PORT} from "../constants/variableNa
 
 export const Payment={
     baseUrl:axios.create({
-        baseUrl:`http://${ONLINE_BACK_END_PORT}:3001/api`
+        baseURL:`http://${ONLINE_BACK_END_PORT}:3001/api`
     }),
     getClientIpAddress: async function (){
         try{
@@ -13,17 +13,17 @@ export const Payment={
             return console.log("Did not get the Ip address ==>", error)
         }
     },
-    init:function(paymentInfo){
-        return this.baseUrl.post("/payment-init", paymentInfo )
+    init: async function(paymentInfo){
+        return await this.baseUrl.post("/payment-init", paymentInfo )
     },
-    stripeInit:function(initInfo){
-         return this.baseUrl.post("/create-payment-intent", initInfo )
+    stripeInit: async function(initInfo){
+         return await this.baseUrl.post("/create-payment-intent", initInfo )
     },
-    validate:function(validateThis){
-        return this.baseUrl.post("/validate", validateThis )
+    validate: async function(validateThis){
+        return await this.baseUrl.post("/validate", validateThis )
     },
-    mobileMoney:function(payloadForMobileMoney){
-        return this.baseUrl.post("/mobile-money-payment", payloadForMobileMoney)
+    mobileMoney: async function(payloadForMobileMoney){
+        return await this.baseUrl.post("/mobile-money-payment", payloadForMobileMoney)
     },
 }
 
